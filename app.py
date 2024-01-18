d1dd48a451ad1505aa923846575ad1067df50862
DiasMath <matheusdiassandes@outlook.com>
2024-01-18 10:49:27 -0300

    atribuindo a variaveis a leitura dos arquivos utilizando pandas

--- app.py
+++ app.py
@@ -2,4 +2,12 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.write("Hello world")
+df_reviews = pd.read_csv("datasets/customer reviews.csv")
+df_top100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
+
+
+
+
+
+
+
