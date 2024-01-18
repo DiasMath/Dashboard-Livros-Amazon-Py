32c236c666a49c6213a65e4d5f7428c37a034ac9
DiasMath <matheusdiassandes@outlook.com>
2024-01-18 11:25:31 -0300

    adicionando formatacao e logica do titulo reviews

---
+++ pages/books_reviews.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Config da tabela para ser wide
+st.set_page_config(layout="wide")
+
+# Atribuicao de variaveis a funcao de ler do pandas
+df_reviews = pd.read_csv("datasets/customer reviews.csv")
+df_top100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
+
+# SelectBox
+books = df_top100_books["book title"].unique()
+book = st.sidebar.selectbox("books", books)
+
+# Reviews - Titulo
+df_book = df_top100_books[df_top100_books["book title"] == book]
+df_book_f = df_reviews[df_reviews["book name"] == book]
+
+book_title = df_book["book title"].iloc[0]
+book_genre = df_book["genre"].iloc[0]
+book_price = f"${df_book['book price'].iloc[0]}"
+book_rating = df_book["rating"].iloc[0]
+book_year = df_book["year of publication"].iloc[0]
+
+st.title(book_title)
+st.subheader(book_genre)
+col1, col2, col3 = st.columns(3)
+
+col1.metric("Price", book_price)
+col2.metric("Rating", book_rating)
+col3.metric("Year of Publication", book_year)
+
+st.divider()
+
+
+
+
+
+
+
